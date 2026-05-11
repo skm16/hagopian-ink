b0a1f0cce0f4ba6766290f9ed81b86804a122e4f
Sean Roberts <sroberts@rarediseases.org>
2026-05-11 15:15:13 -0400

    feat(api-server): register works router

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
+import postsRouter from "./posts";
+import worksRouter from "./works";
 
 const router: IRouter = Router();
 
 router.use(healthRouter);
+router.use(postsRouter);
+router.use(worksRouter);
 
 export default router;
